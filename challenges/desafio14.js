55b5268853598ea91237952834850b8121359039
Kaue Alves <kaue_alves00@yahoo.com>
2022-09-08 16:35:10 -0300

    feat/ concluindo o requisito 15

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,5 +1,4 @@
 db.produtos.find({
-  ingredientes: { $in: ["picles"]},
+  ingredientes: { $in: ["picles"] },
 },
-{ _id: 0, nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 } },
-);
+{ _id: 0, nome: 1, ingredientes: 1, valoresNutricionais: { $slice: 3 } });
